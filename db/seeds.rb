dcb0b39b35afab623276eb22e577dd5668d2a7c6
Korakot Leemakdej <kleemakdej@gmail.com>
2021-07-17 20:12:21 -0700

    bundle

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Beer.create(brand: 'Double Stout', style: 'Stout', country: 'England', quantity: 54)
+Beer.create(brand: 'Spaten', style: 'Helles', country: 'Germany', quantity: 3)
+Beer.create(brand: 'Newcastle', style: 'Brown ale', country: 'UK', quantity: 12)
