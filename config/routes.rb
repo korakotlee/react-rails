20a491613ad9baa21d8b270192684bcbbd3ab21e
Korakot Leemakdej <kleemakdej@gmail.com>
2021-07-18 07:26:50 -0700

    r g controller api/v1/Beers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :beers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'beers#index'
+  namespace :api do
+    namespace :v1 do
+      get 'beers/index'
+      post 'beers/create'
+      delete '/beers/:id', to: 'beers/destroy'
+    end
+  end
 end
