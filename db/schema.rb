baa2bb92df53ea4b3fcef520b2dfb23f5b8a0194
Korakot Leemakdej <kleemakdej@gmail.com>
2021-07-18 06:22:17 -0700

    yarn add antd react-router-dom

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_07_18_031023) do
+
+  create_table "beers", force: :cascade do |t|
+    t.string "brand"
+    t.string "style"
+    t.string "country"
+    t.integer "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
